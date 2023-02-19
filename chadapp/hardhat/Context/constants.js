0d99ae065ad004556270da7cb52e383b8b1218d9
pratzels <prathamkailasiya@gmail.com>
2023-02-19 23:37:33 +0530

    first commit

---
+++ chadapp/hardhat/Context/constants.js
@@ -0,0 +1,7 @@
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+import chadAppJSON from "./ChadApp.json";
+
+export const ChadAppAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+export const ChadAppABI = chadApp.abi;
